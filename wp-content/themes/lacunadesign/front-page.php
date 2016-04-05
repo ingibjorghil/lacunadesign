2934d32842549377222e2a441dc006814a5fa1ce
ingibjorghil <ingibjorg1981@gmail.com>
2016-04-06 00:46:51 +0200

    added cookies law and created skewed div

--- wp-content/themes/lacunadesign/front-page.php
+++ wp-content/themes/lacunadesign/front-page.php
@@ -1,7 +1,18 @@
 <?php get_header(); ?>
-<section>
-	<div class="container">
-		<?php get_template_part( 'woocommerce/content', 'popularproducts' ); ?>
+<section id="popular-item">
+	<div class="wrapper-skew">
+		<div class="wrapper-content">
+			<div class="container">
+				<?php get_template_part( 'woocommerce/content', 'popularproducts' ); ?>
+			</div>
+		</div>
 	</div>
+</section>
+<section>
+	<div class="wrapper-content">
+			<div class="container">
+				<?php get_template_part( 'woocommerce/content', 'popularproducts' ); ?>
+			</div>
+		</div>
 </section>
 <?php get_footer(); ?>
--- wp-content/themes/lacunadesign/woocommerce/content-popularproducts.php
+++ wp-content/themes/lacunadesign/woocommerce/content-popularproducts.php
@@ -1,3 +1,5 @@
+<h2>Populær varer</h2>
+
 <?php
 	$args = array(
 	'post_type' => 'product',
@@ -5,7 +7,6 @@
 	'meta_key' => 'total_sales',
 	'orderby' => 'meta_value_num',
 	);
-
 	$loop = new WP_Query( $args );
 		if ( $loop->have_posts() ) {
 			while ( $loop->have_posts() ) : $loop->the_post();
