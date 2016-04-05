378d6ba31750fd12552c6176622497710895cfa8
ingibjorghil <ingibjorg1981@gmail.com>
2016-04-05 19:19:13 +0200

    ..

--- wp-content/themes/lacunadesign/woocommerce/content-popularproducts.php
+++ wp-content/themes/lacunadesign/woocommerce/content-popularproducts.php
@@ -1,20 +1,18 @@
 <?php
-$args = array(
-'post_type' => 'product',
-'posts_per_page' => 4,
-'meta_key' => 'total_sales',
-'orderby' => 'meta_value_num',
-);
+	$args = array(
+	'post_type' => 'product',
+	'posts_per_page' => 4,
+	'meta_key' => 'total_sales',
+	'orderby' => 'meta_value_num',
+	);
 
-$loop = new WP_Query( $args );
-if ( $loop->have_posts() ) {
-while ( $loop->have_posts() ) : $loop->the_post();
-woocommerce_get_template_part( 'content', 'product' );
-endwhile;
-} else {
-echo __( 'No products found' );
-}
-
-wp_reset_query();
-
+	$loop = new WP_Query( $args );
+		if ( $loop->have_posts() ) {
+			while ( $loop->have_posts() ) : $loop->the_post();
+				woocommerce_get_template_part( 'content', 'product' );
+			endwhile;
+		} else {
+		echo __( 'No products found' );
+		}
+	wp_reset_query();
 ?>
